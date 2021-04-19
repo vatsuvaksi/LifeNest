b7685a6ef0f97959aef6cb1f4f41a322ec8d044a
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-04-20 00:34:04 +0530

    Added more dependency for the front-end and initialised the App as to be called from the index.js

---
+++ server/index.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';  //“CORS” stands for Cross-Origin Resource Sharing. It allows you to make requests from one website to another website in the browser, which is normally prohibited by another browser policy called the Same-Origin Policy (SOP).
