fbe7cd3644b67b01824a26f94d6d42fe3feeaeaa
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-04-20 06:48:29 +0530

    Added a Schema and created a file structure - Controller,Model etc so that the app looks clean as well as scalable

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 const App = () => {
-    return(
-       <div>
-           <h1>App</h1>
-       </div>
-        );
-}
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,28 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors'; 
- /*“CORS” stands for Cross-Origin Resource Sharing. It allows you to make requests 
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/post.js";
+/*“CORS” stands for Cross-Origin Resource Sharing. It allows you to make requests 
  from one website to another website in the browser, which is normally prohibited by another browser policy 
  called the Same-Origin Policy (SOP).
 */
+const app = express();
 
-const app =express();
-
+app.use("/posts", postRoutes); //Through this every route using port is going to start with /posts
 /*
 "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded
 "Limit" is used to parse the maximum size 
 */
-app.use(bodyParser.json({limit : "30mb" , extended: true})); 
-app.use(bodyParser.urlencoded({limit : "30mb" , extended: true})); 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors);
 
-const CONNECTION_URL ="mongodb+srv://vatsuvaksi:Passme123@lifenest-mern.icxcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ;
+const CONNECTION_URL =
+  "mongodb+srv://vatsuvaksi:Passme123@lifenest-mern.icxcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const PORT = process.env.port || 5000 ;
-mongoose.connect(CONNECTION_URL , {useNewUrlParser : true , useUnifiedTopology : true})
-.then(() => app.listen(PORT , () => console.log(`Server running on port ${PORT}`)))
-.catch((err) => console.log(err.message));
+const PORT = process.env.port || 5000;
+mongoose //Creating connection
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
 
-
-mongoose.set('useFindAndModify' , false);
+mongoose.set("useFindAndModify", false); // To remove deprecated function warnings
