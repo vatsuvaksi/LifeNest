6924b40f5c63ac9882d0940fd6c69ef4eec6f1ef
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-04-20 08:23:26 +0530

    Created the file structure for front-end and added the front page using material-ui/Core

---
+++ client/front-end/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post/post";
+const Posts = () => {
+  return (
+    <>
+      <h1>Post</h1>;
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
